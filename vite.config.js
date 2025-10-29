e87759873eede9fa976da4c727eeccbe20d77bc3
luka <luka.gabelaia743@cve.tsu.edu.ge>
2025-10-29 11:36:29 +0400

    მისამართის გამარტივება

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+    },
+  },
 })
